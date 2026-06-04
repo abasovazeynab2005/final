248dce0683056bb2d975fd19570fcd6926179fa5
anasovazeynab <abasovvazeynab2005@gmail.com>
2026-06-04 18:01:10 +0400

    nothing to commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let allStories = null;
+let currentCharacter = null;
+
+const BIN_1_URL = "https://api.jsonbin.io/v3/b/6a2142acda38895dfe8577af/latest";
+const BIN_2_URL   = "https://api.jsonbin.io/v3/b/6a21438eda38895dfe857b6c/latest";
+
